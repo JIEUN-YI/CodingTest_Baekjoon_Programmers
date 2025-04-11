106311f359ef1edab2764ef3b5bfa0baa4ee2f28
JIEUN-YI <yijeun.dev@gmail.com>
2025-04-11 11:38:35 +0900

    [Bronze V] Title: 구구단, Time: 52 ms, Memory: 5756 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.cs
@@ -0,0 +1,5 @@
+            int.TryParse(Console.ReadLine(), out int num);
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine($"{num} * {i} = {num * i}");
+            }
