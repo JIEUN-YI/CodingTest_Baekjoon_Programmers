a99a4fca94f4776dac1630633e3d642560d88f96
JIEUN-YI <yijeun.dev@gmail.com>
2025-04-15 18:33:54 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    int result = -1;
+if(num1 == num2)
+{
+    result = 1;
+}
+else
+{
+    result = -1;
+}
+return result;
+}
