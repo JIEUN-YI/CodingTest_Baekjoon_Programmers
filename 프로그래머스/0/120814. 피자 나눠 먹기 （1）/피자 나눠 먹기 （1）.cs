af3689fdefbc179d56d80e11e2f7de82279f5554
JIEUN-YI <yijeun.dev@gmail.com>
2025-04-16 21:37:50 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.21 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int result;
+        if( n % 7 == 0)
+        {
+            result = n / 7;
+        }
+        else
+        {
+            result = n / 7 + 1 ;
+        }
+        return result;
+    }
+}
