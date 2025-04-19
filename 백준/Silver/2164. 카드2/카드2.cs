c827bc74f319de66b730adc346be1d0ef5ed4ade
JIEUN-YI <yijeun.dev@gmail.com>
2025-04-20 01:31:07 +0900

    [Silver IV] Title: 카드2, Time: 64 ms, Memory: 7684 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.cs
+++ 백준/Silver/2164. 카드2/카드2.cs
@@ -16,9 +16,7 @@ namespace ConsoleApp1
             while(queue.Count > 1)
             {
                 queue.Dequeue(); // 맨 앞에 삭제
-                int i = queue.Peek(); // 그 다음 숫자를
-                queue.Dequeue(); // 삭제 후
-                queue.Enqueue(i); // 삽입
+                queue.Enqueue(queue.Dequeue()); // 그 다음 수를 삭제 후 재 삽입
             }
             
             Console.WriteLine(queue.Peek());
