1934419efa22b7ce60892f6da292c85440182149
JIEUN-YI <yijeun.dev@gmail.com>
2025-04-23 11:07:08 +0900

    [level 0] Title: 개미 군단, Time: 0.28 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+            int result = 0;
+        if (n >= 5)
+        {
+            result = n / 5;
+            n = n % 5;
+        }
+        if (n >= 3)
+        {
+            result += n / 3;
+            n = n % 3;
+        }
+        if (n >= 1)
+        {
+            result += n / 1;
+           n = n % 1;
+        }
+
+        return result;
+    }
+}
