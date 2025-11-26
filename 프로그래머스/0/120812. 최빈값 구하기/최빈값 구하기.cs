75cb950964c4e1a7e9b80404bbd4ca4d83b6055c
JIEUN-YI <yijeun.dev@gmail.com>
2025-11-26 22:48:43 +0900

    [level 0] Title: 최빈값 구하기, Time: 1.84 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.cs
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.cs
@@ -1,26 +1,49 @@
 using System;
-using System.Linq;
+using System.Collections.Generic;
 
 public class Solution {
     public int solution(int[] array) {
-            int[] find = new int[array.Max() + 1];
-
-            foreach(int i in array)
+                    Dictionary<int, int> dic = new Dictionary<int, int>();
+            for (int i = 0; i < array.Length; i++)
             {
-                find[i] += 1;
-            }
-
-            int maxCount = find.Max();
-            int count = 0;
-            foreach(int num in find)
-            {
-                if(num == maxCount)
+                int countNum = 0;
+                for (int j = 0; j < array.Length; j++)
                 {
+                    if (!dic.ContainsKey(array[i])) // 탐색해서 저장하지 않은 값만
+                    {
+                        if (array[i] == array[j])
+                        {
+                            countNum++;
+                        }
+                        else continue;
+                    }
+                    else break;
+                }
+                if(!dic.ContainsKey(array[i]))
+                {
+                    dic.Add(array[i], countNum);
+                }
+            }
+            int maxNum = 0;
+            int key = 0;
+            int count = 0;
+            foreach(int i in dic.Values)
+            {
+                maxNum = Math.Max(maxNum, i);
+            }
+            foreach(int i in dic.Keys)
+            {
+                dic.TryGetValue(i, out int value);
+                if ( value == maxNum)
+                {
+                    key = i;
                     count++;
                 }
             }
-            
-            if(count == 1) { return Array.IndexOf(find, maxCount); }
-            else { return -1; }
+            if(count > 1)
+            {
+                return -1;
+            }
+            return key;
     }
 }
