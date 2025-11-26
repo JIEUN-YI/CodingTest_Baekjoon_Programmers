b1f77b60b5654c0db2260cc278302673f33a7772
JIEUN-YI <yijeun.dev@gmail.com>
2025-11-26 21:38:49 +0900

    [level 0] Title: 주사위의 개수, Time: 0.18 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.cs
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.cs
@@ -2,16 +2,11 @@ using System;
 
 public class Solution {
     public int solution(int[] box, int n) {
-        int[] countAnwer = new int[3];
-for(int i = 0; i < box.Length; i++)
-{
-    countAnwer[i] = box[i] / n; //각 변에 필요한 주사위 갯수 저장
-}
-int answer = 1;
-for (int i = 0; i<countAnwer.Length; i++)
-{
-    answer *= countAnwer[i];
-}
-        return answer;
+                    int multiply = 1;
+            foreach(int num in box)
+            {
+                multiply *= num / n;
+            }
+            return multiply;
     }
 }
