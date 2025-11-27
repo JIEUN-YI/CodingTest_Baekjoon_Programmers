245e054274fb475ad50d85a129b0431875d65895
JIEUN-YI <yijeun.dev@gmail.com>
2025-11-27 23:50:52 +0900

    [level 0] Title: 369게임, Time: 0.68 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.cs
+++ 프로그래머스/0/120891. 369게임/369게임.cs
@@ -1,19 +1,8 @@
 using System;
+using System.Linq;
 
 public class Solution {
     public int solution(int order) {
-                    int count = 0;
-            while (order > 0)
-            {
-                int num = order % 10; // 나머지로 일의 자리 수 구하기
-                order /= 10; // 몫을 order에 저장
-
-                // num이 3, 6, 9인 경우 count 증가 
-                if(num == 3 || num ==6 || num == 9)
-                {
-                    count++;
-                }
-            }
-            return count;
+            return order.ToString().Count(x => x == '3' || x == '6' || x == '9');
     }
 }
