ea6aaa4ee885763703276cd202c446e92edeebae
JIEUN-YI <yijeun.dev@gmail.com>
2025-11-29 23:32:36 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 1.91 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.cs
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.cs
@@ -1,19 +1,8 @@
 using System;
-using System.Text.RegularExpressions;
+using System.Linq;
 
 public class Solution {
     public int solution(string my_string) { 
-           
-            int sum = 0;
-            foreach(char a in my_string)
-            {
-                // char 가 유니코드 숫자인지 판별
-                if (Char.IsNumber(a))
-                {
-                    // GetNumericValue : 숫자 형식의 유니코드 문자를 숫자로 변환
-                    sum += (int)Char.GetNumericValue(a);
-                }
-            }
-            return sum;
+                       return my_string.Where(str => char.IsNumber(str)).Sum(x => Convert.ToInt32(x.ToString()));
     }
 }
