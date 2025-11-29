4e1546f273dc7accdddbd1e867536de864e6b02d
JIEUN-YI <yijeun.dev@gmail.com>
2025-11-29 17:30:09 +0900

    [level 0] Title: 369게임, Time: 0.61 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.cs
+++ 프로그래머스/0/120891. 369게임/369게임.cs
@@ -1,8 +1,13 @@
 using System;
-using System.Linq;
 
 public class Solution {
     public int solution(int order) {
-            return order.ToString().Count(x => x == '3' || x == '6' || x == '9');
+            string input = order.ToString();
+            int count = 0;
+            foreach(char i in input)
+            {
+                if(i == '3' || i == '6' || i == '9') { count++; }
+            }
+            return count;
     }
 }
