34f48eeec63cacb54208e25f7930fbefc4043d7d
JIEUN-YI <yijeun.dev@gmail.com>
2025-11-29 21:28:54 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.68 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.cs
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.cs
@@ -1,27 +1,23 @@
 using System;
+using System.Collections.Generic;
 
 public class Solution {
     public int[] solution(int[] numbers, string direction) {
-                   int[] answer = new int[] { };
+                               List<int> list = new List<int>();
             switch (direction)
             {
                 case "right":
-                    answer = new int[numbers.Length];
-                    answer[0] = numbers[numbers.Length-1];
-                    for(int i = 1; i < numbers.Length; i++)
-                    {
-                        answer[i] = numbers[i-1];
-                    }
+                    list.Add(numbers[numbers.Length - 1]);
+                    for(int i = 0; i < numbers.Length - 1; i++) { list.Add(numbers[i]); }
                     break;
                 case "left":
-                    answer = new int[numbers.Length];
-                    answer[numbers.Length-1] = numbers[0];
-                    for(int i = 1;i < numbers.Length; i++)
-                    {
-                        answer[i-1] = numbers[i];
-                    }
+                    for (int i = 1; i < numbers.Length; i++) { list.Add(numbers[i]); }
+                    list.Add(numbers[0]);
                     break;
+                default:
+                    break;
+
             }
-            return answer;
+            return list.ToArray();
     }
 }
