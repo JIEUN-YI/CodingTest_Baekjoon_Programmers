417d0f340893a8ebb227b3f79faf147d51adda36
JIEUN-YI <yijeun.dev@gmail.com>
2025-11-30 00:07:56 +0900

    [level 1] Title: 하샤드 수, Time: 0.19 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public bool solution(int x) {
+                    int sum = 0;
+            int num = x;
+            while(num > 0)
+            {
+                sum += num % 10;
+                num /= 10;
+            }
+            if (x % sum == 0) { return true; }
+            else { return false; }
+    }
+}
