c9a4eea1f4d1cf2db4b18727051514f613209149
JIEUN-YI <yijeun.dev@gmail.com>
2025-12-01 22:15:55 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.40 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.cs
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.cs
@@ -2,20 +2,6 @@ using System;
 
 public class Solution {
     public int solution(int[] num_list) {
-            int count = 0;
-            int minor = 1;
-            foreach (int num in num_list)
-            {
-                if (num < 0)
-                {
-                    minor = num;
-                    break;
-                }
-            }
-            if (minor == 1)
-            {
-                return -1;
-            }
-            else { return Array.IndexOf(num_list, minor); }
+            return Array.FindIndex(num_list, x => x < 0);
     }
 }
