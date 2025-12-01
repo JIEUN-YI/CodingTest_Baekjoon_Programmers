824ee8da87167015182d044a7f212423b037dea5
JIEUN-YI <yijeun.dev@gmail.com>
2025-12-01 22:03:18 +0900

    [level 0] Title: 커피 심부름, Time: 0.39 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.cs
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.cs
@@ -2,7 +2,18 @@ using System;
 
 public class Solution {
     public int solution(string[] order) {
-        int answer = 0;
-        return answer;
+            int sum = 0;
+            foreach(string item in order)
+            {
+                if (item.Contains("cafelatte"))
+                {
+                    sum += 5000;
+                }
+                else
+                {
+                    sum += 4500;
+                }
+            }
+            return sum;
     }
 }
