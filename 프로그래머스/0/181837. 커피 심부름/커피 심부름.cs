1b2f92bcf7976fb47f5f0f066da7d5812a2dda90
JIEUN-YI <yijeun.dev@gmail.com>
2025-12-01 22:00:27 +0900

    [level 0] Title: 커피 심부름, Time: 2.82 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.cs
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.cs
@@ -1,29 +1,8 @@
 using System;
-using System.Collections.Generic;
 
 public class Solution {
     public int solution(string[] order) {
-                        Dictionary<string, int> menus = new Dictionary<string, int>();
-            menus.Add("iceamericano", 4500);
-            menus.Add("americanoice", 4500);
-            menus.Add("hotamericano", 4500);
-            menus.Add("americanohot", 4500);
-            menus.Add("icecafelatte", 5000);
-            menus.Add("cafelatteice", 5000);
-            menus.Add("hotcafelatte", 5000);
-            menus.Add("cafelattehot", 5000);
-            menus.Add("americano", 4500);
-            menus.Add("cafelatte", 5000);
-            menus.Add("anything", 4500);
-
-            int sum = 0;
-            foreach(string item in order)
-            {
-                if (menus.ContainsKey(item))
-                {
-                    sum+= menus[item];
-                }
-            }
-            return sum;
+        int answer = 0;
+        return answer;
     }
 }
