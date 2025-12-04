742ebdd35ddc7a9f96cb37a50187de8014c1e6d9
JIEUN-YI <yijeun.dev@gmail.com>
2025-12-04 22:47:17 +0900

    [level 1] Title: 소수 찾기, Time: 11.99 ms, Memory: 31.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.cs
@@ -0,0 +1,35 @@
+using System;
+public class Solution {
+    public int solution(int n) {
+            // 소수 이면 true
+            bool[] primeNums = new bool[n + 1];
+            primeNums[0] = false;
+            primeNums[1] = false;
+
+            // 2 이상은 모두 소수라고 가정하고 시작
+            for (int i = 2; i < primeNums.Length; i++)
+            {
+                primeNums[i] = true;
+            }
+
+            for (int index = 2; index < Math.Sqrt(primeNums.Length); index++)
+            {
+                // index의 제곱수 이전의 수는 이미 다른 수에 의해서 지워짐. 따라서 그 이상부터 검토
+                //  += index를 통해 index의 배수만 찾기
+                for (long i = index * index; i < primeNums.Length; i += index)
+                {
+                    // 나눠지는 경우 소수가 아님
+                    primeNums[i] = false;
+                }
+
+            }
+
+            int count = 0;
+            foreach (bool a in primeNums)
+            {
+                if (a) count++;
+            }
+
+            return count;
+    }
+}
