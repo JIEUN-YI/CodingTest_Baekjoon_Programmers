7b048d15c633d0bae9e65122f2be972f5cc28ace
JIEUN-YI <yijeun.dev@gmail.com>
2025-12-07 22:48:32 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 1.97 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public string solution(string s) {
+            char[] chars = s.ToCharArray();
+            Array.Sort(chars);
+            Array.Reverse(chars);
+            string answer = new string(chars);
+            return answer;
+    }
+}
