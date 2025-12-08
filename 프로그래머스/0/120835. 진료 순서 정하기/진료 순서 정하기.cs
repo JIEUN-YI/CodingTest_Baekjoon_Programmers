9301c76a6cf5872e82f97c85a6afd03045456aaf
JIEUN-YI <yijeun.dev@gmail.com>
2025-12-08 22:36:12 +0900

    [level 0] Title: 진료 순서 정하기, Time: 2.88 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.cs
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.cs
@@ -3,8 +3,20 @@ using System.Linq;
 
 public class Solution {
     public int[] solution(int[] emergency) {
-            int[] order = emergency.OrderByDescending(x => x).ToArray();
+            int[] order = new int[emergency.Length];
+            for (int i = 0; i < emergency.Length; i++)
+            {
+                order[i] = emergency[i];
+            }
 
-            return emergency.Select(x=>Array.IndexOf(order,x)+1).ToArray();
+            Array.Sort(order);
+            Array.Reverse(order);
+
+            int[] answer = new int[order.Length];
+            for(int i = 0; i < order.Length; i++)
+            {
+                answer[i] = Array.IndexOf(order, emergency[i]) + 1;
+            }
+            return answer;
     }
 }
