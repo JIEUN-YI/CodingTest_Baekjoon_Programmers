6f0a3fb98fbaf31d0690966dbebecc4a67138cf5
JIEUN-YI <yijeun.dev@gmail.com>
2025-12-21 00:40:00 +0900

    [level 0] Title: 최빈값 구하기, Time: 3.40 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.cs
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.cs
@@ -4,17 +4,19 @@ using System.Linq;
 
 public class Solution {
     public int solution(int[] array) {
-                    // 키 = array의 값, 값 = 같은 수 의 개수
-            Dictionary<int, int> dic = new Dictionary<int, int>();
+                    Dictionary<int, int> dic = new Dictionary<int, int>();
 
             foreach (int item in array)
             {
+                // key가 이미 Dictionary에 있다면
                 if (dic.ContainsKey(item))
                 {
+                    // 갯수를 +1 하고 값에 저장
                     dic[item] += 1;
                 }
                 else
                 {
+                    // Dictionary에 없다면 새로 key를 저장하고 value를 1로 저장
                     dic.Add(item, 1);
                 }
             }
@@ -22,12 +24,16 @@ public class Solution {
             int maxNum = dic.Values.Max(); // 값의 최대값 찾기
 
             int count = 0;
+            // Dictionary 값 중에서 최대값과 같은 값이 있는지를 확인하기
             foreach (int i in dic.Values)
             {
-                if(i == maxNum) { count++; }
+                if (i == maxNum) { count++; }
             }
 
-            if (count == 1) { return dic.FirstOrDefault(x => x.Value == maxNum).Key; }
+            // 최빈값이 하나라면 그 값의 키를 반환
+            if (count == 1) { return dic.SingleOrDefault(x => x.Value == maxNum).Key; }
+            //if (count == 1) { return dic.FirstOrDefault(x => x.Value == maxNum).Key; }
+            // 최빈값이 여러개면 -1 반환
             else { return -1; }
     }
 }
