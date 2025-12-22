3e88d02033348ef6b8f0243745d8bb69ac070780
JIEUN-YI <yijeun.dev@gmail.com>
2025-12-22 21:59:11 +0900

    [Bronze V] Title: A+B, Time: 48 ms, Memory: 5476 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cs
@@ -0,0 +1,19 @@
+
+using System.Text;
+
+namespace ConsoleStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+            string[] input = sr.ReadLine().Split(" ");
+            int.TryParse(input[0], out int a); 
+            int.TryParse(input[1], out int b);
+
+            Console.WriteLine((a + b).ToString());
+        }
+    }
+}
