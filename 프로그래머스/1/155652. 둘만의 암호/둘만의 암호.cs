2f9d85f2a7067cf29db029838408d6c46cb412e6
JIEUN-YI <yijeun.dev@gmail.com>
2025-12-25 22:24:22 +0900

    [level 1] Title: 둘만의 암호, Time: 0.35 ms, Memory: 30.7 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.cs
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.cs
@@ -4,40 +4,32 @@ using System.Text;
 public class Solution {
     public string solution(string s, string skip, int index) {
             // 알파벳 맵핑 'a' = 97 == 0번째 인덱스
-            int[] mapping = new int[26];
-            foreach(char c in skip)
+            bool[] mapping = new bool[26];
+            foreach (char c in skip)
             {
                 // 제외해야하는 알파벳 표시
-                if(mapping[c-'a'] == 0)
+                if (mapping[c - 'a'] == false)
                 {
-                    mapping[c - 'a'] = 1;
+                    mapping[c - 'a'] = true;
                 }
             }
             StringBuilder sb = new StringBuilder();
             // 암호문을 탐색
-            foreach(char c in s)
+            foreach (char c in s)
             {
                 // 기준이 되는 char 배열의 인덱스 값
                 int key = c - 'a';
                 // 찾을 횟수
-                int count = 0; 
+                int count = 0;
 
                 while (count < index)
                 {
-                    // key+1이 범위를 벗어나면
-                    if (key + 1 >= mapping.Length)
-                    { 
-                        //다시 시작
-                        key = -1;
-                    }
-                    
-                    // 통과여부확인
-                    if (mapping[key + 1] == 0)
+                    // key 범위를 벗어나지 않도록 루프
+                    key = (key + 1) % 26;
+                    if (!mapping[key])
                     {
-                        key++;
                         count++;
                     }
-                    else { key++; continue; }
                 }
                 sb.Append((char)(key + 'a'));
             }
