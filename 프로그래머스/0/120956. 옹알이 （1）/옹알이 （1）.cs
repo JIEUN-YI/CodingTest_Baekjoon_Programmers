91648255ccd653913eef416384b342265ec97c65
JIEUN-YI <yijeun.dev@gmail.com>
2025-12-25 17:04:18 +0900

    [level 0] Title: 옹알이 (1), Time: 0.61 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.cs
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.cs
@@ -3,25 +3,20 @@ using System;
 public class Solution {
     public int solution(string[] babbling) {
             string[] words = { "aya", "ye", "woo", "ma" };
-            for(int i = 0; i < babbling.Length; i++)
+            for (int i = 0; i < babbling.Length; i++)
             {
-                foreach(string word in words)
+                foreach (string word in words)
                 {
-                    if (babbling[i].Contains(word))
-                    {
-                        babbling[i] = babbling[i].Replace(word, " ");
-                    }
+                    babbling[i] = babbling[i].Replace(word, " ");
                 }
+                babbling[i] = babbling[i].Replace(" ", "");
 
             }
-            for(int i =0;i<babbling.Length; i++)
-            {
-                babbling[i] = babbling[i].Replace(" ","");
-            }
+
             int count = 0;
-            foreach(string word in babbling)
+            foreach (string word in babbling)
             {
-                if(word == "")
+                if (word == "")
                 {
                     count++;
                 }
