190b7039f54f6f7a3ebd803893bb43a844b5dfc9
JIEUN-YI <yijeun.dev@gmail.com>
2025-12-27 23:54:31 +0900

    [level 0] Title: 접미사 배열, Time: 4.25 ms, Memory: 31.7 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.cs
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.cs
@@ -3,17 +3,24 @@ using System.Collections.Generic;
 
 public class Solution {
     public string[] solution(string my_string) {
+            // 접미사를 저장할 배열
             string[] suffixs = new string[my_string.Length];
-            int count = 0;
-            while (count < my_string.Length)
+            // 접미사를 저장할 배열의 인덱스
+            int index = 0;
+            // 총 my_string의 길이만큼 반복
+            while (index < my_string.Length)
             {
-                List<char> suffix = new List<char>(my_string.Length - count);
-                for(int index = count; index < suffixs.Length; index++)
+                // 현재 접미사를 만들 리스트
+                List<char> suffix = new List<char>(my_string.Length);
+                // 시작지점 : index 값 = 처음에는 0번 위치부터 my_string.Length-1 인덱스까지 출발 지점
+                for (int i = index; i < my_string.Length; i++)
                 {
-                    suffix.Add(my_string[index]);
+                    suffix.Add(my_string[i]);
                 }
-                suffixs[count] = new string(suffix.ToArray());
-                count++;
+                // 리스트에 완성된 접미사를 배열에 저장
+                suffixs[index] = new string(suffix.ToArray());
+                // 인덱스 증가
+                index++;
             }
             Array.Sort(suffixs);
 
