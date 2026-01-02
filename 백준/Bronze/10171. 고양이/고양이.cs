ae71ed3319d5fa89d1c31c73134681c4cd3c0b69
JIEUN-YI <yijeun.dev@gmail.com>
2026-01-02 23:46:54 +0900

    [Bronze V] Title: 고양이, Time: 44 ms, Memory: 5320 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cs
@@ -0,0 +1,15 @@
+using System.Text;
+
+namespace ConsoleStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\\    /\\");
+            Console.WriteLine(" )  ( ')");
+            Console.WriteLine("(  /  )");
+            Console.WriteLine(" \\(__)|");
+        }
+    }
+}
