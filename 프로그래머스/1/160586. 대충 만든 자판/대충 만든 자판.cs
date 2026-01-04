a3d4c3f6f5f5e484174a0cb9e10bfaaf97e3c4e1
JIEUN-YI <yijeun.dev@gmail.com>
2026-01-04 21:37:48 +0900

    [level 1] Title: 대충 만든 자판, Time: 0.52 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.cs
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.cs
@@ -3,27 +3,29 @@ using System.Collections.Generic;
 
 public class Solution {
     public int[] solution(string[] keymap, string[] targets) {
-            int[] map = new int[28];
+            int[] map = new int[26];
             List<int> list = new List<int>(100);
-            for (int i = 0; i < keymap.Length; i++)
+
+            foreach(string key in keymap)
             {
                 int count = 1;
-                foreach (char c in keymap[i])
+                foreach(char c in key)
                 {
-                    if (map[c - 65] != 0)
+                    if (map[c - 'A'] != 0)
                     {
-                        map[c - 65] = Math.Min(count, map[c - 65]);
+                        map[c - 'A'] = Math.Min(count, map[c - 'A']);
                     }
-                    else { map[c - 65] = count; }
+                    else { map[c - 'A'] = count; }
                     count++;
                 }
             }
+
             foreach (string str in targets)
             {
                 int sum = 0;
                 foreach (char c in str)
                 {
-                    if (map[c - 65] != 0) { sum += map[c - 65]; }
+                    if (map[c - 'A'] != 0) { sum += map[c - 'A']; }
                     else { sum = -1; break; }
                 }
                 list.Add(sum);
