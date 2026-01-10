3bfbb0378555dedf2d3ed93b0ba5853bcda353fe
JIEUN-YI <yijeun.dev@gmail.com>
2026-01-10 23:39:18 +0900

    [level 2] Title: 피보나치 수, Time: 1.60 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int solution(int n) {
+            int[] fibonacci = new int[n + 1];
+            fibonacci[0] = 0;
+            fibonacci[1] = 1;
+
+            for (int index = 2; index < n + 1; index++)
+            {
+                fibonacci[index] = fibonacci[index - 1] % 1234567 + fibonacci[index - 2] % 1234567;
+            }
+
+            return fibonacci[fibonacci.Length - 1]% 1234567;
+    }
+}
