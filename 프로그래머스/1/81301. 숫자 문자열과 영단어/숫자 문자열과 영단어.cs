70e9b2b22117a2ef33668dd825b57ce230c1d3f5
JIEUN-YI <yijeun.dev@gmail.com>
2026-01-25 23:14:24 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 1.70 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cs
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cs
@@ -2,17 +2,14 @@ using System;
 
 public class Solution {
     public int solution(string s) {
-                    string[] changes = new string[]
+            string[] changes = new string[]
             {
                 "zero", "one", "two", "three", "four",
                 "five", "six", "seven", "eight", "nine"
             };
-            for(int index = 0; index < changes.Length; index++)
+            for (int index = 0; index < changes.Length; index++)
             {
-                if (s.Contains(changes[index]))
-                {
-                    s = s.Replace(changes[index], index.ToString());
-                }
+                s = s.Replace(changes[index], index.ToString());
             }
             return int.Parse(s);
     }
