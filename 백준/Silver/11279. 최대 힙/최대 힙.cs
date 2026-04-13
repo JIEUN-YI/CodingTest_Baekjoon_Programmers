113248e94ec3594934d86f7f4b585aa6d81d8179
JIEUN-YI <yijeun.dev@gmail.com>
2026-04-13 21:43:36 +0900

    [Silver II] Title: 최대 힙, Time: 100 ms, Memory: 12428 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cs
@@ -0,0 +1,40 @@
+using System.Text;
+
+namespace Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(Console.OpenStandardInput());
+            StringBuilder sb = new StringBuilder();
+            int.TryParse(sr.ReadLine(), out int N);
+
+            PriorityQueue<int,int> minHeap = new PriorityQueue<int,int>();
+            while(N-->0)
+            {
+                // 현재 값
+                int.TryParse(sr.ReadLine(), out int value);
+                if(value == 0)
+                {
+                    if(minHeap.Count == 0)
+                    {
+                        // 0 출력
+                        sb.AppendLine("0");
+                    }
+                    else
+                    {
+                        // 최대 값 출력
+                        sb.AppendLine(minHeap.Dequeue().ToString());
+                    }
+                }
+                else // 큐에 저장
+                {
+                    // 최대힙으로 사용하기 위한 우선순위 : * -1
+                    minHeap.Enqueue(value, value * -1);
+                }
+            }
+            Console.WriteLine(sb.ToString());   
+        }
+    }
+}
